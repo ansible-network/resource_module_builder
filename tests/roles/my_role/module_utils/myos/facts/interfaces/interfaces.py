8cc414396e77b4a6b1938017514e02feca0a4def
Bradley A. Thornton <bthornto@thethorntons.net>
2019-05-23 12:18:58 -0700

    add tests, recursion

--- tests/roles/my_role/module_utils/myos/facts/interfaces/interfaces.py
+++ tests/roles/my_role/module_utils/myos/facts/interfaces/interfaces.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# {{ rm['COPYRIGHT'] }}
+# Copyright 2019 Red Hat
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """
-The {{ network_os }} {{ resource }} fact class
+The myos interfaces fact class
 It is in this file the configuration is collected from the device
 for a given resource, parsed, and the facts tree is populated
 based on the configuration.
 """
 import re
 from copy import deepcopy
-from {{ import_path }}. \
-     {{ network_os }}.facts.base import FactsBase
+from ansible.module_utils. \
+     myos.facts.base import FactsBase
 
-class {{ resource|capitalize }}Facts(FactsBase):
-    """ The {{ network_os }} {{ resource }} fact class
+class InterfacesFacts(FactsBase):
+    """ The myos interfaces fact class
     """
 
     def populate_facts(self, module, connection, data=None):
-        """ Populate the facts for {{ resource }}
+        """ Populate the facts for interfaces
         :param module: the module instance
         :param connection: the device connection
         :param data: previously collected conf
