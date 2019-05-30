c44974dd4df275a06fc6bce8bf9f564b05eff209
Bradley A. Thornton <bthornto@thethorntons.net>
2019-05-30 10:08:56 -0700

    Remove test formatter

--- roles/resource_module/filter_plugins/to_argspec.py
+++ roles/resource_module/filter_plugins/to_argspec.py
@@ -40,29 +40,6 @@ def dive(obj, result):
             result[k]['options'] = dict()
             dive(suboptions, result[k]['options'])
 
-def pretty(value, htchar='\t', lfchar='\n', indent=0):
-    nlch = lfchar + htchar * (indent + 1)
-    if type(value) is dict:
-        items = [
-            nlch + repr(key) + ': ' + pretty(value[key], htchar, lfchar, indent + 1)
-            for key in value
-        ]
-        return '{%s}' % (','.join(items) + lfchar + htchar * indent)
-    elif type(value) is list:
-        items = [
-            nlch + pretty(item, htchar, lfchar, indent + 1)
-            for item in value
-        ]
-        return '[%s]' % (','.join(items) + lfchar + htchar * indent)
-    elif type(value) is tuple:
-        items = [
-            nlch + pretty(item, htchar, lfchar, indent + 1)
-            for item in value
-        ]
-        return '(%s)' % (','.join(items) + lfchar + htchar * indent)
-    else:
-        return repr(value)
-
 def to_argspec(spec):
     if 'DOCUMENTATION' not in spec:
         raise AnsibleFilterError("missing required element 'DOCUMENTATION'"
